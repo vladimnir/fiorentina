d168f210ee0f155dc4486cd38197c44f8eee4285
vladimnir <vladimir@wagento.com>
2016-05-17 16:13:25 -0400

    primer commit

---
+++ app/code/Vlad/Foo/Block/Foot.php
@@ -0,0 +1,14 @@
+<?php
+namespace Vlad\Foo\Block;
+
+/**
+* Baz block
+*/
+class Foot
+    extends \Magento\Framework\View\Element\Template
+{
+    public function getTitle()
+    {
+        return "AYUDA";
+    }
+}
